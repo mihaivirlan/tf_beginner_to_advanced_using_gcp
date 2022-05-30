8038ce7452e6155e6854c2f1b8c4ee7836ed5978
Mihai Virlan <Mihai.virlan@ibm.com>
2022-05-30 18:10:42 +0300

    working with terraform variables

---
+++ first_instance/main.tf
@@ -0,0 +1,27 @@
+resource "google_compute_instance" "default" {
+  name = "mymachine12345"
+  // Example with usage of variables
+  machine_type = var.machine_type
+  zone = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+        // Example with usage of variables
+        image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+// Example of outputs
+output "machine_type" { value = "${google_compute_instance.default.machine_type}" }
+output "name" { value = "${google_compute_instance.default.name}" }
+output "zone" { value = "${google_compute_instance.default.zone}" }
+output "boot_disk" { value = "${google_compute_instance.default.boot_disk[0].initialize_params[0].image}" }
+output "network_interface" { value = "${google_compute_instance.default.network_interface[0].network}" }
---
+++ first_instance/variable.tf
@@ -0,0 +1,2 @@
+variable "image" { default = "debian-9-stretch-v20220303" }
+variable "machine_type" { default = "f1-micro" }
